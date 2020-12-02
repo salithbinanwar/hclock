12147bdf8d900f86366175b70c47cf417095114d
salith bin anwar <62356981+salithbinanwar@users.noreply.github.com>
2020-12-02 19:58:41 +0600

    first commit

--- exp.js
+++ exp.js
@@ -8,4 +8,3 @@ var n = d.getMinutes();  document.getElementById("minute").innerHTML = n;
     var d = new Date();
 var n = d.getSeconds();  document.getElementById("second").innerHTML = n;
 }, 1000);
-
